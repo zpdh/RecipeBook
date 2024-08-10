8741d9dcb3536a708e7056909d8c746cdc4b641f
Antunes <36316038+zpdh@users.noreply.github.com>
2024-08-10 09:53:55 -0300

    feat: add recipe registration use cases

--- src/Backend/RecipeBook.Application/DependencyInjectionExtension.cs
+++ src/Backend/RecipeBook.Application/DependencyInjectionExtension.cs
@@ -3,10 +3,12 @@ using Microsoft.Extensions.DependencyInjection;
 using RecipeBook.Application.Services.AutoMapper;
 using RecipeBook.Application.UseCases.Login;
 using RecipeBook.Application.UseCases.Login.ExecuteLogin;
+using RecipeBook.Application.UseCases.Recipe.Register;
 using RecipeBook.Application.UseCases.User.ChangePassword;
 using RecipeBook.Application.UseCases.User.Profile;
 using RecipeBook.Application.UseCases.User.Registration;
 using RecipeBook.Application.UseCases.User.Update;
+using Sqids;
 
 namespace RecipeBook.Application;
 
@@ -14,7 +16,7 @@ public static class DependencyInjectionExtension
 {
     public static void AddApplication(this IServiceCollection serviceCollection, IConfiguration configuration)
     {
-        AddAutoMapper(serviceCollection);
+        AddAutoMapper(serviceCollection, configuration);
         AddUseCases(serviceCollection);
     }
 
@@ -25,13 +27,20 @@ public static class DependencyInjectionExtension
         serviceCollection.AddScoped<IGetUserProfileUseCase, GetUserProfileUseCase>();
         serviceCollection.AddScoped<IUpdateUserUseCase, UpdateUserUseCase>();
         serviceCollection.AddScoped<IChangePasswordUseCase, ChangePasswordUseCase>();
+        serviceCollection.AddScoped<IRegisterRecipeUseCase, RegisterRecipeUseCase>();
     }
 
-    private static void AddAutoMapper(IServiceCollection serviceCollection)
+    private static void AddAutoMapper(IServiceCollection serviceCollection, IConfiguration configuration)
     {
+        var sqids = new SqidsEncoder<long>(new SqidsOptions
+        {
+            MinLength = 10,
+            Alphabet = configuration.GetValue<string>("Settings:CryptographyAlphabet")!
+        });
+        
         serviceCollection.AddScoped(provider => new AutoMapper.MapperConfiguration(options =>
         {
-            options.AddProfile(new AutoMapping());
+            options.AddProfile(new AutoMapping(sqids));
         }).CreateMapper());
     }
 }
