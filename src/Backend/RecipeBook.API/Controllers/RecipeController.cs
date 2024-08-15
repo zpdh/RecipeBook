fc32144635ef5b2e3eb65a2d5ec93473b480debd
Antunes <36316038+zpdh@users.noreply.github.com>
2024-08-15 04:45:53 -0300

    feat: add endpoint to update recipe

--- src/Backend/RecipeBook.API/Controllers/RecipeController.cs
+++ src/Backend/RecipeBook.API/Controllers/RecipeController.cs
@@ -5,6 +5,7 @@ using RecipeBook.Application.UseCases.Recipe.Delete;
 using RecipeBook.Application.UseCases.Recipe.Filter;
 using RecipeBook.Application.UseCases.Recipe.GetById;
 using RecipeBook.Application.UseCases.Recipe.Register;
+using RecipeBook.Application.UseCases.Recipe.Update;
 using RecipeBook.Communication.Requests;
 using RecipeBook.Communication.Responses;
 
@@ -48,8 +49,7 @@ public class RecipeController : RecipeBookBaseController
     [ProducesResponseType(typeof(ErrorResponseJson), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> GetById(
         [FromServices] IGetRecipeByIdUseCase useCase,
-        [FromRoute] [ModelBinder(typeof(IdBinder))]
-        long id)
+        [FromRoute] [ModelBinder(typeof(IdBinder))] long id)
     {
         var response = await useCase.Execute(id);
 
@@ -62,10 +62,23 @@ public class RecipeController : RecipeBookBaseController
     [ProducesResponseType(typeof(ErrorResponseJson), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Delete(
         [FromServices] IDeleteRecipeUseCase useCase,
-        [FromRoute] [ModelBinder(typeof(IdBinder))]
-        long id)
+        [FromRoute] [ModelBinder(typeof(IdBinder))] long id)
     {
         await useCase.Execute(id);
+
+        return NoContent();
+    }
+
+    [HttpPut]
+    [Route("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ErrorResponseJson), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update(
+        [FromServices] IUpdateRecipeUseCase useCase,
+        [FromRoute] [ModelBinder(typeof(IdBinder))] long id,
+        [FromBody] RecipeRequestJson request)
+    {
+        await useCase.Execute(id, request);
 
         return NoContent();
     }
