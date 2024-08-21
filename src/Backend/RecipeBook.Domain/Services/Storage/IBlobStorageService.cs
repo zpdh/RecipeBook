6b6130f49249c362bd04ba716aca58342d476132
Antunes <36316038+zpdh@users.noreply.github.com>
2024-08-21 14:57:53 -0300

    refactor: make recipe get by id method return image url

--- src/Backend/RecipeBook.Domain/Services/Storage/IBlobStorageService.cs
+++ src/Backend/RecipeBook.Domain/Services/Storage/IBlobStorageService.cs
@@ -5,4 +5,5 @@ namespace RecipeBook.Domain.Services.Storage;
 public interface IBlobStorageService
 {
     Task Upload(User user, Stream file, string fileName);
+    Task<string> GetImageUrl(User user, string imageIdentifier);
 }
--- src/Backend/RecipeBook.Infrastructure/Services/Storage/AzureStorageService.cs
+++ src/Backend/RecipeBook.Infrastructure/Services/Storage/AzureStorageService.cs
@@ -1,6 +1,9 @@
 using Azure.Storage.Blobs;
+using Azure.Storage.Sas;
 using RecipeBook.Domain.Entities;
+using RecipeBook.Domain.Extensions;
 using RecipeBook.Domain.Services.Storage;
+using RecipeBook.Domain.ValueObjects;
 
 namespace RecipeBook.Infrastructure.Services.Storage;
 
@@ -15,10 +18,43 @@ public class AzureStorageService : IBlobStorageService
 
     public async Task Upload(User user, Stream file, string fileName)
     {
-        var container = _blobServiceClient.GetBlobContainerClient(user.UserIdentifier.ToString());
-        await container.CreateIfNotExistsAsync();
+        var containerClient = _blobServiceClient.GetBlobContainerClient(user.UserIdentifier.ToString());
+        await containerClient.CreateIfNotExistsAsync();
 
-        var blobClient = container.GetBlobClient(fileName);
+        var blobClient = containerClient.GetBlobClient(fileName);
         await blobClient.UploadAsync(file, overwrite: true);
+    }
+
+    public async Task<string> GetImageUrl(User user, string fileName)
+    {
+        var containerName = user.UserIdentifier.ToString();
+        var containerClient = _blobServiceClient.GetBlobContainerClient(user.UserIdentifier.ToString());
+
+        var exists = await containerClient.ExistsAsync();
+
+        if (exists.Value.IsFalse())
+        {
+            return string.Empty;
+        }
+
+        var blobClient = containerClient.GetBlobClient(fileName);
+        exists = await blobClient.ExistsAsync();
+
+        if (exists.Value.IsFalse())
+        {
+            return string.Empty;
+        }
+
+        var sasBuilder = new BlobSasBuilder
+        {
+            BlobContainerName = containerName,
+            BlobName = fileName,
+            Resource = "b",
+            ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(RuleConstants.MaximumImageUrlLifetimeInMinutes)
+        };
+
+        sasBuilder.SetPermissions(BlobSasPermissions.Read);
+
+        return blobClient.GenerateSasUri(sasBuilder).ToString();
     }
 }
